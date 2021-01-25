3282dad5dc80b6fde333a24967f0154097d5339d
Salas123 <salasii.jesus@gmail.com>
2021-01-25 15:40:58 -0800

    updated Navbar with responsive side bar and imported google font into indexhtml

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <>
+        <Router>
+            <Navbar/>
+        </Router>
+      </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from "react";
+import './Navbar.css';
+import {Link} from "react-router-dom";
+
+function Navbar (){
+
+    const[click, setClick] = useState(false);
+
+    const handleMenuClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false);
+
+    return(
+        <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/intro' className='navbar-logo' onClick={closeMobileMenu}>
+                    JSII
+                </Link>
+            </div>
+            <div className='menu-icon' onClick={handleMenuClick}>
+                <i className={click ? "fas fa-minus" : "fas fa-ellipsis-v"}/>
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                    <li className="nav-item">
+                        <Link to='/intro' className='nav-links' onClick={closeMobileMenu}>
+                            About Me
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/projects' className='nav-links' onClick={closeMobileMenu}>
+                            Projects
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/contactme' className='nav-links' onClick={closeMobileMenu}>
+                            Contact Me
+                        </Link>
+                    </li>
+            </ul>
+        </nav>
+        </>
+    );
+}
+
+
+export default Navbar;
