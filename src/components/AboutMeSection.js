4d110461e55d22052fdeaa3a8ed3a3787ec22162
Salas123 <salasii.jesus@gmail.com>
2021-03-04 15:20:21 -0800

    updated some responsive text features

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -9,8 +9,7 @@ const AboutMeSection = () =>{
             <div className='about-me-text-container'>
                 <p>
                     I am a first-generation son of two Mexican immigrants. I grew up both in California and Minnesota.
-                </p>
-                <p>
+
                     Some of my accomplishments include receiving a scholarship for Latinos In Technology and
                     attending/graduating from UC Riverside with a Bachelors in Science: Computer Science. While I have a profound interest in computer interest, my other interests
                     involve creating shorts
