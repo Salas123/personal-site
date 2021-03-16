2b4c3c5163100679ee215ab1a5785a4be49f0327
Salas123 <salasii.jesus@gmail.com>
2021-03-16 15:04:02 -0700

    updated about me text section and added contact text forum that is comprised of a textfield for full name, email, and a message body

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import './ContactMeSection.css';
+import {Button, Container, Grid, TextField} from "@material-ui/core";
+// import {makeStyles} from "@material-ui/styles";
+
+// const useStyles = makeStyles((theme) => ({
+//     root: {
+//         '& > *': {
+//             margin: theme.spacing(1),
+//             width: '25ch',
+//         },
+//     },
+// }));
 
 const ContactMeSection = () =>{
+
+    // const classes = useStyles();
+
+
     return(
         <div className='contact-container' id="ContactMeSection">
-            <h1 className='contact-me-header'>CONTACT ME</h1>
-            <div className='container-divider'/>
-            <div className='contact-me-text-container'>
-                <p>
-                    I am always looking for new and interesting projects to collaborate on. For any inquiries or to view my resume
-                    please email me at my email below and I'll get back to you!
-                </p>
-                <div className="email-text-wrapper">
-                    <p>
-                        Email:
-                    </p>
-                    <p className="email-text-paragraph">
-                        salasii@gmail.com
-                    </p>
-                </div>
-
-            </div>
+            <Container className="contact-me-form-container"  fixed>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="center"
+                        alignItems="stretch">
+                        <h1 className='contact-me-header'>CONTACT ME</h1>
+                        <div className="contact-me-underline"/>
+                        <div className='contact-me-text-container'>
+                            <p>
+                                I am always looking for new and interesting projects to collaborate on. For any inquiries or to view my resume
+                                please email me at my email below and I'll get back to you!
+                            </p>
+                        </div>
+                        <form noValidate autoComplete="on">
+                            <TextField className="textfield-component" id="standard-basic" label="Your Full Name Here" color="secondary" margin="normal" variant="filled" InputProps={{className:"textfield__body"}} InputLabelProps={{className: "textfield__label"}}/>
+                            <TextField className="textfield-component" id="standard-basic" label="Your Email Here"  color="secondary" margin="normal" variant="filled" InputProps={{className:"textfield__body"}} InputLabelProps={{className: "textfield__label"}}/>
+                        </form>
+                        <form noValidate autoComplete="off">
+                            <TextField
+                                className="textfield-component"
+                                id="outlined-multiline-static"
+                                label="Message"
+                                color="secondary"
+                                multiline
+                                rows={4}
+                                defaultValue="Type Message Here..."
+                                variant="outlined"
+                                margin="normal"
+                                InputProps={{className:"textfield-message-body"}} InputLabelProps={{className: "textfield__label"}}
+                            />
+                        </form>
+                        <Button className="submit-button" variant="outlined" color="secondary">Submit</Button>
+                    </Grid>
+            </Container>
         </div>
     );
 }
