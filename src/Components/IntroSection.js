c2e1f2d076112ca8692dddc80e38d2e773f34791
Salas123 <salasii.jesus@gmail.com>
2022-02-15 11:08:06 -0800

    adding previous depreciated folder and initial scheme for intro section

---
+++ src/Components/IntroSection.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const IntroSection = () => {
+    return(
+        <div className={"IntroSectionContainer"}>
+            <div className={"leftContainer"}>
+                <h1 className={"fullNameHeader"}>Jesus Salas II</h1>
+                <p className={"personParaDescriptor"}></p>
+            </div>
+            <div className={"rightContainer"}>
+            </div>
+        </div>
+    );
+};
+
+export default IntroSection;
