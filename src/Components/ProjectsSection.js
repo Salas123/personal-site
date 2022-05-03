92356b511d4bb9a28daae478e9a9198627357a0f
Salas123 <salasii.jesus@gmail.com>
2022-05-03 14:55:22 -0700

    changing file extensions for new image formats

--- src/Components/ProjectsSection.js
+++ src/Components/ProjectsSection.js
@@ -17,12 +17,12 @@ const ProjectsSection = () =>{
                 <div className={'ProjectsMiddleBoxContainer'}>
                     <h2 className={'ProjectsSubHeader'}>Photography</h2>
                     <ul className={'ProjectsListWrapper'}>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/brooklyn-apts1.jpeg'} alt={'apartments in brooklyn'} buttonText={'Full Image'}>Location: Brooklyn, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/nyc-apartments1.jpeg'} alt={'apartments in nyc'} buttonText={'Full Image'}> Location: Manhattan, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/nyc-street-art.jpeg'} alt={'mural in the nyc street'} buttonText={'Full Image'}> Location: Manhattan, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/sheep-england.jpeg'} alt={'sheep near stone hedge'} buttonText={'Full Image'}>Location: Stone Hedge, England. Camera: Sony Alpha 6000.</CardComponent></li>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/barcelona-streets.jpeg'} alt={'streets of barcelona'} buttonText={'Full Image'}> Location: Barcelona, Spain. Camera: Sony Alpha 6000.</CardComponent></li>
-                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/barcelona-overview.jpeg'} alt={'overview of Barcelona'} buttonText={'Full Image'}> Location: Barcelona, Spain. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/brooklyn-apts1.png'} alt={'apartments in brooklyn'} buttonText={'Full Image'}>Location: Brooklyn, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/nyc-apartments1.png'} alt={'apartments in nyc'} buttonText={'Full Image'}> Location: Manhattan, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/nyc-street-art.png'} alt={'mural in the nyc street'} buttonText={'Full Image'}> Location: Manhattan, NYC. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/sheep-england.png'} alt={'sheep near stone hedge'} buttonText={'Full Image'}>Location: Stone Hedge, England. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/barcelona-streets.png'} alt={'streets of barcelona'} buttonText={'Full Image'}> Location: Barcelona, Spain. Camera: Sony Alpha 6000.</CardComponent></li>
+                        <li className={'ProjectsElement'}><CardComponent photoUrl={'artifacts/barcelona-overview.png'} alt={'overview of Barcelona'} buttonText={'Full Image'}> Location: Barcelona, Spain. Camera: Sony Alpha 6000.</CardComponent></li>
                     </ul>
                     <h2 className={'ProjectsSubHeader'}>Engineering</h2>
                     <ul className={'ProjectsListWrapper'}>
