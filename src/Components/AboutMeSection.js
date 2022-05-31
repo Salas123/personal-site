6fb194692554760121ba570d872c24f2d3c134fe
Salas123 <salasii.jesus@gmail.com>
2022-05-30 19:57:00 -0700

    updating about me paragraph

--- src/Components/AboutMeSection.js
+++ src/Components/AboutMeSection.js
@@ -30,14 +30,12 @@ function AboutMeSection(){
                         <p className={'AboutMeParagraph'}>
                             My name is Jesus Salas II. I made this page initially as a resume buffer, however, upon further reflection I realize I
                             have the chance to create a platform around myself. I am proud to say that I graduated with my bachelors of science in computer science from
-                            UC Riverside and along with graduating I worked on some great embedded systems projects as
-                            well as worked on some non-CS work like photography,cl and video editing. As I'm slowly progressing in my career with Software Engineering,
-                            I am also still working on other side projects.
+                            UC Riverside where I worked on some great embedded systems projects. Since graduation, I have taken it upon myself to learn more about full stack as
+                            well as continuing my non-engineering projects in photography and video editing.
                         </p>
                         <p className={'AboutMeParagraph'}>
                             I love to create and I love the collaborative process that comes with it. I find the idea of a shared vision and optimism to create
-                            something that generates inspiration in others to do same to be a very rewarding endeavor. My goal in this platform is to hopefully at least get
-                            close to that endeavor and to hopefully meet with others down the line and collaborate on shared projects.
+                            something that generates inspiration in others to do same as a rewarding endeavor. My goal in this platform is to hopefully continue to pursue that endeavor and to hopefully meet with others down the line to collaborate on shared projects.
                         </p>
                     </div>
                 </div>
